a0946390e78d5c363b9ededff8d7ad0b1b5240c6
walkingaltair <osman.minaz@outlook.de>
2020-05-22 16:47:39 +0200

    insertable locked numbers

--- app_class.py
+++ app_class.py
@@ -1,24 +1,37 @@
 import pygame, sys
 from settings import *
+from buttonClass import *
 
 class App:
     def __init__(self):
         pygame.init()
         self.window = pygame.display.set_mode((WIDTH, HEIGHT))
         self.running = True
-        self.grid = testBoard
+        self.grid = testBoard2
         self.selected = None
         self.mousePos = None
+        self.state = "playing"
+        self.playingButtons = []
+        self.menuButtons = []
+        self.endButtons = []
+        self.lockedCells = []
+        self.font = pygame.font.SysFont("arial", cellSize//2)
+        self.loadButtons()
+
 
     def run(self):
         while self.running:
-            self.events()
-            self.update()
-            self.draw()
+            if self.state == "playing":
+                self.playing_events()
+                self.playing_update()
+                self.playing_draw()
         pygame.quit()
         sys.exit()
 
-    def events(self):
+##### playing state functions ###########
+
+
+    def playing_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
@@ -30,15 +43,34 @@ class App:
                     print("not on the board")
                     self.selected = None
 
-    def update(self):
+    def playing_update(self):
         self.mousePos = pygame.mouse.get_pos()
 
-    def draw(self):
+        for button in self.playingButtons:
+            button.update(self.mousePos)
+
+    def playing_draw(self):
         self.window.fill(WHITE)
+
+        for button in self.playingButtons:
+            button.draw(self.window)
+
         if self.selected:
             self.drawSelection(self.window, self.selected)
+
+        self.drawNumbers(self.window)
+
         self.drawGrid(self.window)
         pygame.display.update()
+
+##### Helper functions ######
+
+    def drawNumbers(self, window):
+        for yidx, row in enumerate(self.grid):
+            for xidx, num in enumerate(row):
+                if num != 0:
+                    pos = [xidx*cellSize+gridPos[0], yidx*cellSize+gridPos[1]]
+                    self.textToScreen(window, str(num), pos)
 
     def drawSelection(self, window, pos):
         pygame.draw.rect(window, LIGHTBLUE, ((pos[0]*cellSize)+gridPos[0], (pos[1]*cellSize)+gridPos[1], cellSize, cellSize))
@@ -46,12 +78,8 @@ class App:
     def drawGrid(self, window):
         pygame.draw.rect(window, BLACK, (gridPos[0], gridPos[1], WIDTH-150, HEIGHT-150), 2)
         for x in range(9):
-            if x % 3 != 0:
-                pygame.draw.line(window, BLACK, (gridPos[0]+(x*cellSize), gridPos[1]), (gridPos[0]+(x*cellSize), gridPos[1]+450))
-                pygame.draw.line(window, BLACK, (gridPos[0], gridPos[1]+(x*cellSize)), (gridPos[0]+450, gridPos[1]+(x*cellSize)))
-            else:
-                pygame.draw.line(window, BLACK, (gridPos[0], gridPos[1]+(x*cellSize)), (gridPos[0]+450, gridPos[1]+(x*cellSize)), 2)
-                pygame.draw.line(window, BLACK, (gridPos[0]+(x*cellSize), gridPos[1]), (gridPos[0]+(x*cellSize), gridPos[1]+450), 2)
+            pygame.draw.line(window, BLACK, (gridPos[0], gridPos[1]+(x*cellSize)), (gridPos[0]+450, gridPos[1]+(x*cellSize)), 2 if x % 3 == 0 else 1)
+            pygame.draw.line(window, BLACK, (gridPos[0]+(x*cellSize), gridPos[1]), (gridPos[0]+(x*cellSize), gridPos[1]+450), 2 if x % 3 == 0 else 1)
 
     def mouseOnGrid(self):
         if self.mousePos[0] < gridPos[0] or self.mousePos[1] < gridPos[1]:
@@ -60,3 +88,14 @@ class App:
             return False
 
         return ((self.mousePos[0]-gridPos[0])//cellSize, (self.mousePos[1]-gridPos[1])//cellSize)
+
+    def loadButtons(self):
+        self.playingButtons.append(Button(20,40, 100, 40))
+
+    def textToScreen(self, window, text, pos):
+        font = self.font.render(text, False, BLACK)
+        fontWidth = font.get_width()
+        fontHeight = font.get_height()
+        pos[0] += (cellSize - fontWidth)//2
+        pos[1] += (cellSize - fontHeight)//2
+        window.blit(font, pos)

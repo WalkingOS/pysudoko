d6537da473f64b7b4f811048df28bd30f94f3cd9
walkingaltair <osman.minaz@outlook.de>
2020-05-22 15:01:04 +0200

    insesrted selection

--- app_class.py
+++ app_class.py
@@ -7,7 +7,8 @@ class App:
         self.window = pygame.display.set_mode((WIDTH, HEIGHT))
         self.running = True
         self.grid = testBoard
-        print(self.grid)
+        self.selected = None
+        self.mousePos = None
 
     def run(self):
         while self.running:
@@ -21,14 +22,26 @@ class App:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                selected = self.mouseOnGrid()
+                if selected:
+                    self.selected = selected
+                else:
+                    print("not on the board")
+                    self.selected = None
 
     def update(self):
-        pass
+        self.mousePos = pygame.mouse.get_pos()
 
     def draw(self):
         self.window.fill(WHITE)
+        if self.selected:
+            self.drawSelection(self.window, self.selected)
         self.drawGrid(self.window)
         pygame.display.update()
+
+    def drawSelection(self, window, pos):
+        pygame.draw.rect(window, LIGHTBLUE, ((pos[0]*cellSize)+gridPos[0], (pos[1]*cellSize)+gridPos[1], cellSize, cellSize))
 
     def drawGrid(self, window):
         pygame.draw.rect(window, BLACK, (gridPos[0], gridPos[1], WIDTH-150, HEIGHT-150), 2)
@@ -39,3 +52,11 @@ class App:
             else:
                 pygame.draw.line(window, BLACK, (gridPos[0], gridPos[1]+(x*cellSize)), (gridPos[0]+450, gridPos[1]+(x*cellSize)), 2)
                 pygame.draw.line(window, BLACK, (gridPos[0]+(x*cellSize), gridPos[1]), (gridPos[0]+(x*cellSize), gridPos[1]+450), 2)
+
+    def mouseOnGrid(self):
+        if self.mousePos[0] < gridPos[0] or self.mousePos[1] < gridPos[1]:
+            return False
+        if self.mousePos[0] > gridPos[0]+gridSize or self.mousePos[1] > gridPos[1]+gridSize:
+            return False
+
+        return ((self.mousePos[0]-gridPos[0])//cellSize, (self.mousePos[1]-gridPos[1])//cellSize)
--- settings.py
+++ settings.py
@@ -3,6 +3,7 @@ HEIGHT = 600
 
 WHITE = (255,255,255)
 BLACK = (0,0,0)
+LIGHTBLUE = (96, 216, 232)
 
 
 testBoard = [[0 for x in range(9)] for x in range(9)]
@@ -10,3 +11,4 @@ testBoard = [[0 for x in range(9)] for x in range(9)]
 
 gridPos = (75, 100)
 cellSize = 50
+gridSize = cellSize*9
